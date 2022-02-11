9a4450797c2b0e3116a2b33e40cdb7829ea96c63
Maksim <maxim2000shev@gmail.com>
2022-02-11 15:50:16 +0300

    servver rest Api

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module StandartWebServer
 
 go 1.17
 
-require github.com/BurntSushi/toml v1.0.0 // indirect
+require (
+	github.com/BurntSushi/toml v1.0.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+)
--- internal/app/api/api.go
+++ internal/app/api/api.go
@@ -1,15 +1,33 @@
 package api
 
+import (
+	"github.com/gorilla/mux"
+
+	"github.com/sirupsen/logrus"
+)
+
 type API struct {
 	config *Config
+	logger *logrus.Logger
+	router *mux.Router
 }
 
 func New(config *Config) *API {
 	return &API{
 		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
 func (api *API) Start() error {
+
+	if err := api.configureLoggerFiled(); err != nil {
+
+		return err
+	}
+	api.logger.Info("starting api server at port:", api.config.BindAddr)
+
+	api.configureRouterFiled()
 	return nil
 }
